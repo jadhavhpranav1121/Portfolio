caa75c4745ad837a58532ad760a51542c8471b01
jadhavhpranav1121 <jadhavhpranav1121@gmail.com>
2022-04-10 19:59:10 +0530

    loader added

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
-
+var preloader=document.getElementById('loading');
+function loader(){
+    preloader.style.display='none';
+}
 $(window).scroll(function() {
     if ($(window).scrollTop() > 0) {
         $('.navigation').addClass('floatingNav');
